70701cce71f8fcd400cfa4ffb61c7f67e9e73632
Sayantan Sarkar <sayskar500@gmail.com>
2022-01-28 02:24:52 +0530

    getAllExpenseByUserId method added

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -1,4 +1,5 @@
 const Expense = require('../models/expenseSchema')
+const User = require('../models/userSchema')
 
 const getExpenseUser = async (req, res) => {
   try {
@@ -11,8 +12,23 @@ const getExpenseUser = async (req, res) => {
 
 const createExpenseUser = async (req, res) => {
   try {
+    const user = await User.findById(req.body.userId)
     const expense = await Expense.create(req.body)
+    const deduct = user.balance - expense.amount
+    user.balance = deduct
+    await user.save()
     res.status(201).json(expense)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const getAllExpenseByUserId = async (req, res) => {
+  try {
+    const all = await Expense.find({
+      userId: req.params.userId,
+    })
+    res.status(200).json(all)
   } catch (error) {
     res.status(500).json(error)
   }
@@ -21,4 +37,5 @@ const createExpenseUser = async (req, res) => {
 module.exports = {
   getExpenseUser,
   createExpenseUser,
+  getAllExpenseByUserId,
 }
--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -2,10 +2,12 @@ const { Router } = require('express')
 const {
   getExpenseUser,
   createExpenseUser,
+  getAllExpenseByUserId,
 } = require('../controllers/expenseController')
 
 const router = Router()
 
+router.get('/getExpenseByUser/:userId', getAllExpenseByUserId)
 router.get('/expense/:id', getExpenseUser)
 router.post('/expense', createExpenseUser)
 
