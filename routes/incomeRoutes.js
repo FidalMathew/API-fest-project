f2cf8a41435c42ae64237eedcdb431b78ef1dbb7
Preetam Sarkar <sarkarpreetam12@gmail.com>
2022-01-28 14:05:00 +0530

    arrangement of route done

--- routes/incomeRoutes.js
+++ routes/incomeRoutes.js
@@ -7,8 +7,8 @@ const {
 
 const router = Router()
 
+router.post('/income', createIncomeUser)
 router.get('/getIncomeByUser/:userId', getAllIncomeByUserId)
 router.get('/income/:id', getIncomeUser)
-router.post('/income', createIncomeUser)
 
 module.exports = router
