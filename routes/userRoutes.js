c102cfb4ca7ccf88eaa3cdc9f8a967e470485528
Preetam Sarkar <sarkarpreetam12@gmail.com>
2022-01-28 20:07:21 +0530

    comment added for user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,10 +10,13 @@ const {checkUser} = require('../middlewares/authMiddlewares')
 
 const router = Router();
 
+// routes for signup and signin
 router.post('/signupAuth',signupAuth);
 router.post('/signinAuth',signinAuth);
 // router.patch('/user/:id',createUser)
+// route for logout
 router.get('/logout',logout);
+//  route for getting all the cashflow
 router.get('/cashflow/:userId',checkUser,getCashflow);
 
 
