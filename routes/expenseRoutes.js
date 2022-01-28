b8a22a7e76940e02a54ee1c946f06ae0bb85d35e
Preetam Sarkar <sarkarpreetam12@gmail.com>
2022-01-28 20:04:46 +0530

    comment added for auth middleware

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -9,8 +9,12 @@ const {checkUser} = require('../middlewares/authMiddlewares')
 
 const router = Router()
 
+// all are protected routes
+// routes for creating expense
 router.post('/expense',checkUser, createExpenseUser)
+// routes for getting all the expenses by particular user _id
 router.get('/getExpenseByUser/:userId',checkUser, getAllExpenseByUserId)
+// getting particular expense by expense id
 router.get('/expense/:id',checkUser, getExpenseUser)
 
 module.exports = router
